96a34bee6a119d041bffd3548f48c60e8cedeb16
kingoftheflame <matthew.l.mcclure186@gmail.com>
2024-08-27 12:39:21 -0500

    added nvim repo as submodule

--- home-manager/matthew/default.nix
+++ home-manager/matthew/default.nix
@@ -16,7 +16,7 @@
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
     ../modules/terminal
-    ../modules/neovim
+    ../modules/nvim
   ];
 
 
