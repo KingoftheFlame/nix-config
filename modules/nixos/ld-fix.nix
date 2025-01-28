ad4d49f933e07cde62324924f05518f839da1976
kingoftheflame <matthew.l.mcclure186@gmail.com>
2025-01-28 13:19:08 -0600

    syntax error fix for ld-fix.nix

--- modules/nixos/ld-fix.nix
+++ modules/nixos/ld-fix.nix
@@ -1,7 +1,7 @@
  { config, pkgs, inputs, outputs,  ... }:
 {
   programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = {
+  programs.nix-ld.libraries = [
     #Add any missing dynamic libraries here instead of in enviroment.systempackages
-  }
+  ];
 }
