0d369ed009e01259a0c180d6c0f7c5baf186708c
kingoftheflame <matthew.l.mcclure186@gmail.com>
2025-03-06 10:15:48 -0600

    added vscode

--- modules/home-manager/creative/development.nix
+++ modules/home-manager/creative/development.nix
@@ -1,8 +1,21 @@
 {pkgs, config, ...}:
 {
   home.packages = with pkgs;[
-    arduino
-    arduino-cli
+    # arduino
+    # arduino-cli
+    arduino-ide
     kdePackages.ghostwriter
+  
   ];
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+   profiles.default.extensions = with pkgs.vscode-extensions; [
+      # dracula-theme.theme-dracula
+      # vscodevim.vim
+      yzhang.markdown-all-in-one
+    ];
+  };
+  
 }
